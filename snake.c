1c691da5e42fa84ead94c7f93483dbac26ef4446
Francis <francisklinck@gmail.com>
2019-10-10 21:03:02 +0200

    code base

--- snake.c
+++ snake.c
@@ -1,7 +1,19 @@
 #include <ncurses.h>
 
-
+typedef struct {
+  unsigned int x;
+  unsigned int y;
+} coord;
 
 int main() {
+  /* init the ncurses screen */
+  initscr();
+  noecho();
+  /* turn off delay on getch */
+  nodelay(stdscr, TRUE);
   
+  getch();
+  /* close the ncurses screen */
+  endwin();
+  return 0;
 }
