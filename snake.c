7d9094e1228f1229d72bf2d166df05fe8100de36
FKD13 <francisklinck@gmail.com>
2019-10-12 14:14:06 +0200

    add direction keys and color

--- snake.c
+++ snake.c
@@ -2,103 +2,121 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct {
-  unsigned int x;
-  unsigned int y;
+    int x;
+    int y;
 } Coord;
 
 typedef struct {
-  Coord **body;
-  unsigned int body_size;
-  unsigned int full_body_size;
-  bool alive;
+    Coord **body;
+    unsigned int body_size;
+    unsigned int full_body_size;
+    bool alive;
 } Snake;
 
 char key_hit(const char key) {
-  int chr = getch();
-  return (chr != ERR ) && ((char) chr) == key;
+    int chr = getch();
+    return chr != ERR && ((char) chr) == key;
 }
 
 /* TODO: check if malloc creation has succeeded */
 void init_snake(Snake *snake) {
-  snake->body = (Coord **) malloc(20*sizeof(Coord *));
-  snake->body[0] = malloc(sizeof(Coord));
-  snake->body[0]->x = 3;
-  snake->body[0]->y = 3;
-  snake->body_size = 1;
-  snake->full_body_size = 20;
-  snake->alive = TRUE;
+    snake->body = (Coord **) malloc(20*sizeof(Coord *));
+    snake->body[0] = malloc(sizeof(Coord));
+    snake->body[0]->x = 3;
+    snake->body[0]->y = 3;
+    snake->body_size = 1;
+    snake->full_body_size = 20;
+    snake->alive = TRUE;
 }
 
 void draw(Snake *snake) {
-  //refresh();
-  //sleep(5);
-  clear();
-  unsigned int i;
-  for (i=0;i<snake->body_size;i++) {
-    Coord *c = snake->body[i];
-    //printw("\n%d %d,", c->x, c->y); 
-    mvprintw(c->y,c->x,"O");
-  }
-  //printw("%d", snake->body_size);
-  refresh();
+    clear();
+    /* select snake color */
+    attron(COLOR_PAIR(1));
+    unsigned int i;
+    for (i=0;i<snake->body_size;i++) {
+        Coord *c = snake->body[i];
+        mvprintw(c->y,c->x,"@");
+    }
+    refresh();
 }
 
 void add_coord(Coord *coord, Coord *direction) {
-  coord->x += direction->x;
-  coord->y += direction->y;
+    coord->x += direction->x;
+    coord->y += direction->y;
 }
 
 /* TODO: check realloc succeeded */
 void move_snake(Snake *snake, Coord *direction) {
-  //printw("%d %d,", direction->x, direction->y); 
-  if (snake->full_body_size < snake->body_size + 1) {
-    snake->body = realloc(snake->body, snake->full_body_size*2*sizeof(Coord));
-    snake->full_body_size *= 2;
-  }
-  
-  snake->body_size += 1;
-  unsigned int i;
-  for (i=snake->body_size-1;i>0;i--) {
-    snake->body[i] = snake->body[i-1];
-  }
-  //for (i=1;i<=snake->body_size;i++) {
-  //  tmp = snake->body[i];
-  //  snake->body[i] = next;
-  //  next = tmp;
-  //}
-  //snake->body[i] = next;
-  
-  Coord *next = snake->body[0];
-  Coord *current = malloc(sizeof(Coord));
-  current->x = next->x;
-  current->y = next->y;
-  add_coord(current, direction);
-  snake->body[0] = current;
+    if (snake->full_body_size < snake->body_size + 1) {
+        snake->body = realloc(snake->body, snake->full_body_size*2*sizeof(Coord));
+        snake->full_body_size *= 2;
+    }
+
+    snake->body_size += 1;
+    unsigned int i;
+    for (i=snake->body_size-1;i>0;i--) {
+        snake->body[i] = snake->body[i-1];
+    }
+
+    Coord *next = snake->body[0];
+    Coord *current = malloc(sizeof(Coord));
+    current->x = next->x;
+    current->y = next->y;
+    add_coord(current, direction);
+    snake->body[0] = current;
 }
 
 int main() {
-  /* init the ncurses screen */
-  initscr();
-  noecho();
-  /* disable cursor */
-  curs_set(0);
-  /* turn off delay on getch */
-  nodelay(stdscr, TRUE);
-  /* main loop */
-  Snake snake;
-  init_snake(&snake);
+    /* init the ncurses screen */
+    initscr();
+    start_color();
+    noecho();
+    /* disable cursor */
+    curs_set(0);
+    /* turn off delay on getch */
+    nodelay(stdscr, TRUE);
+    /* colors */
+    /* snake color */
+    init_pair(1, COLOR_RED, COLOR_RED);
+    /* main loop */
+    time_t last;
+    last = time(0);
 
-  while(!key_hit('\n')) {
+    Snake snake;
+    init_snake(&snake);
     Coord direction = (Coord) {1, 0};
-    move_snake(&snake, &direction);
-    //printw("%d", snake.body_size);
-    draw(&snake);
-    sleep(1);
-  };
-  
-  /* close the ncurses screen */
-  endwin();
-  return 0;
+
+    bool running = TRUE;
+    while(running) {
+        /* handle input */
+        int chr = getch();
+        if (chr != ERR) {
+            char key = (char) chr;
+            if (key == 'a') {
+                direction = (Coord) {-1, 0};
+            } else if (key == 'z') {
+                direction = (Coord) {0, -1};
+            } else if (key == 'd') {
+                direction = (Coord) {1, 0};
+            } else if (key == 's') {
+                direction = (Coord) {0, 1};
+            } else if (key == '\n') {
+                running = FALSE;
+            }
+        }
+        /* step when every second */
+        if ( difftime(time(0), last) >= 1) {
+            last = time(0);
+            move_snake(&snake, &direction);
+            draw(&snake);
+        }
+    };
+
+    /* close the ncurses screen */
+    endwin();
+    return 0;
 }
