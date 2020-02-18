9093bd99172001ebddb084c368c9e53c29d427a3
Francis <francisklinck@gmail.com>
2020-02-18 01:55:35 +0100

    add better main structure

---
+++ src/server/game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fklinck on 2/18/20.
+//
+
+#include "Game.h"
---
+++ src/server/game.h
@@ -0,0 +1,14 @@
+//
+// Created by fklinck on 2/18/20.
+//
+
+#ifndef CNAKE_GAME_H
+#define CNAKE_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //CNAKE_GAME_H
